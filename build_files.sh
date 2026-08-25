ae2e9c2e6da8b114a7fafbc317ef94b29c087bee
John Ansley Rocel <estevesjancen388@gmail.com>
2026-08-25 11:25:41 +0800

    fix(build): add --break-system-packages flag for Vercel Python environment

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Installing dependencies..."
-pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt --break-system-packages
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
