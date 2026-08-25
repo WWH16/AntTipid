e4cfb47065c050c52ca0584423086dc6242f003b
John Ansley Rocel <estevesjancen388@gmail.com>
2026-08-25 11:51:24 +0800

    fix(urls): add /favicon.ico route redirecting to /static/images/AntTipidFavicon.png

--- AntTipid/urls.py
+++ AntTipid/urls.py
@@ -3,10 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/images/AntTipidFavicon.png', permanent=True)),
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
